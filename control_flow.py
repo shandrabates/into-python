4d88b89a0af5bd5a77d2d9585e200d188a04e4a1
shandra <shandra.corbitt@gmail.com>
2019-08-05 11:04:54 -0600

    Updated

--- control_flow.py
+++ control_flow.py
@@ -10,6 +10,21 @@ if True:
 if bool("eggs"):
     print('Yes please!')
 
+if "eggs":
+    print('Yes, why not')
+
+# Flat is better than Nested
+h = 42
+if h>50:
+    print('Greater than 50')
+elif h<50:  #Else If
+    print('Less than 50')
+else:
+    print('Equals 50')
+
+
+
+#####################
 print("Done")
 
 
