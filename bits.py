d4209f6e0585b08674c37f13bf18a01c2702246e
shandra <shandra.corbitt@gmail.com>
2019-08-05 12:54:38 -0600

    Bytes misspelled

---
+++ bits.py
@@ -0,0 +1,12 @@
+'''
+How Python handles bytes
+Learn about bytes.
+Bytes are similar to str type, but they are a sequence of bytes instead of Unicode code points.
+
+Used for raw binary data, fixed-width, single-byte encoding: ASCII
+
+Use the byte() constructor
+'''
+d = b'data'
+print(d, type(d))
+
