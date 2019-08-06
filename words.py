d27fc98b6b5beb1f62ee88f871aa9b81b36404ab
shandra <shandra.corbitt@gmail.com>
2019-08-06 09:08:20 -0600

    Correction to previous functionality

--- words.py
+++ words.py
@@ -6,9 +6,8 @@ Task #1: Count the number of words in the document
 '''
 
 from urllib.request import urlopen
-from functions import even_or_odd
 
-file = "http://icarus.cs.weber.edu/~hvalle/hafb/words.txt"
+filename = "http://icarus.cs.weber.edu/~hvalle/hafb/words.txt"
 
 n = 0
 
@@ -23,7 +22,7 @@ print("total number of words", n)
 
 count = 0
 data ={}
-with urlopen(file) as story:
+with urlopen(filename) as story:
     for line in story:
         lists = line.decode('utf-8').split() #must decode into strings and then separate with spaces
         # print lists
@@ -44,5 +43,3 @@ for key in sorted(data.keys()):
     print(key, data[key])
 
 
-print('Even or Odd word count')
-even_or_odd(count)
