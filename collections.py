233dd2ec98889c0c6c47ce7527721a6b4c670698
shandra <shandra.corbitt@gmail.com>
2019-08-06 14:45:18 -0600

    Random notes

---
+++ collections.py
@@ -0,0 +1,16 @@
+"""
+Learn about collections: Tuples, Strings, Range, List
+"""
+
+
+def main():
+    """
+    test function
+    :return: nothing
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
