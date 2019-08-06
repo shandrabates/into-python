f2877b7d6cd1bc7eea1f4fcc41c5d54a7ed270ba
shandra <shandra.corbitt@gmail.com>
2019-08-06 14:43:55 -0600

    Random notes

---
+++ notes.py
@@ -0,0 +1,12 @@
+"""
+Random class notes
+
+tuple - hererogeneous immutable sequence
+    delimited by () but accessed by []  t[index]
+
+dict - defined by {} and accessed by [] key
+
+str -  defined by " "   and accessed by [] index
+
+list - delimited/defined by [] and accessed by [] index
+"""
--- scope.py
+++ scope.py
@@ -1,6 +1,29 @@
 """
-Python Info HERE:
+Learn about scoping in Python
+
+Probably the best data analysis Python to use that is free is Anaconda package
 """
+
+count = 0    # global object
+
+def show_count():
+    """
+    Disply the current count
+    :return: nothing
+    """
+    print(count)
+
+
+def set_count(para):
+    """
+    Set global counter value to whatever parameter is input
+    :param para: input number
+    :return: nothing
+    """
+    global count    # this makes the variable "count" global
+    count = para    # this becomes a local variable, so it does not change the global
+    print(count)
+
 
 
 def main():
@@ -8,7 +31,9 @@ def main():
     test function
     :return: nothing
     """
-    pass
+    show_count()
+    set_count(2)
+    show_count()
 
 
 if __name__ == '__main__':
