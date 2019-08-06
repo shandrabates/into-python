20b635381acfabb0514b07bbbca2b45d665ee34e
shandra <shandra.corbitt@gmail.com>
2019-08-06 11:27:40 -0600

    Reference functions

---
+++ references.py
@@ -0,0 +1,44 @@
+"""
+Learn about references and how Python uses them (ex: how Python points to things)
+"""
+
+
+def modify(k):
+    """
+    Modify the content of a list
+    :param k: input list
+    :return: nothing
+    """
+    # lists are passed by reference
+    k.append(39)
+    print("k = ", k)
+
+
+def replace(g):
+    """
+    Replace input list
+    :param g: input list
+    :return: nothing
+    """
+    g = [17, 48, 89]
+    print("g = ", g)
+
+
+
+
+def main():
+    """
+    test function
+    :return: nothing
+    """
+    m = [9, 15, 24]
+    print("Before modify() m = ", m)
+    modify(m)
+    print("After modify() m = ", m)
+    replace(m)
+    print("After replace() m = ", m)
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
