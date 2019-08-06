86cdea1080689fb836563bc344bb01e68c20303f
shandra <shandra.corbitt@gmail.com>
2019-08-06 13:50:51 -0600

    Create a border message

--- border.py
+++ border.py
@@ -2,6 +2,7 @@
 Python Info HERE:
 """
 
+import time
 
 def banner(message, border='*'):
     """
@@ -10,9 +11,22 @@ def banner(message, border='*'):
     :param border: border character for string
     :return: printed banner
     """
+    print('\n')  # add some space before the banner printing
     print(border*len(message)+4*border)
-    print(border,message,border)
+    print(border, message, border)
     print(border*len(message)+4*border)
+
+
+def add_spam(menu=None):
+    """
+    Add spam to the menu list
+    :param menu: an input parameter or Python's version of null
+    :return: menu list
+    """
+    if menu is None:
+        menu = []
+    menu.append('spam')
+    return menu
 
 
 def main():
@@ -24,6 +38,12 @@ def main():
     banner('Weber State', '-')
     # Use the banner function without the optional border parameter
     banner('University of Utah')
+    print(add_spam())
+
+    breakfast = ['eggs', 'bacon']
+    print('Before', breakfast)
+    add_spam(breakfast)
+    print("After", breakfast)
 
 
 if __name__ == '__main__':
