5583233204502835c71b3fe3df29310ba21d2540
shandra <shandra.corbitt@gmail.com>
2019-08-08 09:21:24 -0600

    Learning about generators

--- gen.py
+++ gen.py
@@ -29,13 +29,35 @@ def run_take():
         print(item)
 
 
+def distinct(iterable):
+    """
+    Return unique items by eliminating duplicates
+    :param iterable: The source series
+    :yields: Unique elements in order from 'iterable'
+    """
+    seen = set()
+    for item in iterable:
+        if item in seen:
+            continue
+        yield item
+        seen.add(item)
+        # print(seen) to see this being built/run
+
+
+def run_distinct():
+    items = [5, 7, 7, 6, 5, 5]
+    for item in distinct(items):
+        print(item)
+
+
 
 def main():
     """
     test function
     :return: nothing
     """
-    run_take()
+    #run_take()
+    run_distinct()
 
 
 if __name__ == '__main__':
