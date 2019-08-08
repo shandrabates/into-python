8591317e55fc2625fbba674da44f558596c3d940
shandra <shandra.corbitt@gmail.com>
2019-08-08 10:40:14 -0600

    Learning about generators

---
+++ my_iterations.py
@@ -0,0 +1,35 @@
+"""
+When working with iterations, generators, etc
+Look at the documentation for the itertools module
+
+"""
+
+from itertools import islice, count
+
+from list_comps import is_prime
+
+def main():
+    """
+    test function
+    :return: nothing
+    """
+    ten_thousand_primes = islice((x for x in count() if is_prime(x)), 10000)
+    print(ten_thousand_primes, type(ten_thousand_primes))
+    # This is useful to do the operation quickly, depending on how many operations you want to do
+    print('List of first 10k prime numbers:', list(ten_thousand_primes))
+    # Note: If you need to use the object again, you need to re-generate it
+    ten_thousand_primes = islice((x for x in count() if is_prime(x)), 10000)
+    print('Sum of first 100k prime numbers:', sum(ten_thousand_primes))
+    # Other built-ins use with itertools: any, all
+    print(any([False, False, True]))  # Or -> are any of these the same?
+    print(all([False, False, True]))  # And - > are all of these the same?
+    # Are there any prime numbers in the range 1328 to 1361
+    print('There are prime numbers between 1328 and 1361?', any(x for x in range(1328, 1362) if is_prime(x)))
+    # Another method
+    print(any(is_prime(x) for x in range(1328, 1362)))
+    print(list(x for x in range(1328, 1362) if is_prime(x)))
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
